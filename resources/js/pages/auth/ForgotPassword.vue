6bfb98021d1e99735ec0336ab392752356814f28
Jose Terrazas <jozaguts@gmail.com>
2025-10-21 09:32:48 -0600

    fix(auth): login show route

--- resources/js/pages/auth/ForgotPassword.vue
+++ resources/js/pages/auth/ForgotPassword.vue
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AuthLayout from '@/layouts/AuthLayout.vue';
-import { login } from '@/routes';
+import auth from '@/routes/auth';
 import { email } from '@/routes/password';
 import { Form, Head } from '@inertiajs/vue3';
 import { LoaderCircle } from 'lucide-vue-next';
@@ -61,7 +61,7 @@ defineProps<{
 
             <div class="space-x-1 text-center text-sm text-muted-foreground">
                 <span>Or, return to</span>
-                <TextLink :href="login()">log in</TextLink>
+                <TextLink :href="auth.login.show()">log in</TextLink>
             </div>
         </div>
     </AuthLayout>
