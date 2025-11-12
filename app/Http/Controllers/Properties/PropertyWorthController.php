5a658b36227500174278acc3e0d4042f687c29b5
Jose Terrazas <jozaguts@gmail.com>
2025-11-11 18:46:40 -0600

    feat(PropertyWorthController):MonthlyPropertyUsageService added

--- app/Http/Controllers/Properties/PropertyWorthController.php
+++ app/Http/Controllers/Properties/PropertyWorthController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Properties;
 
+use App\Application\Usage\Services\MonthlyPropertyUsageService;
+use App\Domain\Shared\Exceptions\FeatureLimitExceededException;
+use App\Domain\Usage\Enums\UsageAction;
 use App\Http\Controllers\Controller;
 use App\Models\Property;
+use App\Models\User;
 use App\Services\Appraisal\PropertyWorthService;
 use Illuminate\Http\RedirectResponse;
 
@@ -11,10 +15,18 @@ class PropertyWorthController extends Controller
 {
     public function __construct(
         protected PropertyWorthService $service,
+        private readonly MonthlyPropertyUsageService $usageService,
     ) {}
 
+    /**
+     * @throws FeatureLimitExceededException
+     */
     public function fetch(Property $property): RedirectResponse
     {
+        $user = request()->user();
+        if ($user instanceof User) {
+            $this->usageService->ensureUsage($user, $property, UsageAction::APPRAISAL);
+        }
         $this->service->fetch($property);
 
         return redirect()
@@ -24,6 +36,18 @@ class PropertyWorthController extends Controller
 
     public function report(Property $property): RedirectResponse
     {
+        $user = request()->user();
+
+        if ($user instanceof User) {
+            try {
+                $this->usageService->ensureUsage($user, $property, UsageAction::REPORT);
+            } catch (FeatureLimitExceededException $exception) {
+                return redirect()
+                    ->route('properties.show', $property)
+                    ->withErrors(['worth' => $exception->getMessage()]);
+            }
+        }
+
         $worth = $property->latestWorth;
 
         if (! $worth) {
