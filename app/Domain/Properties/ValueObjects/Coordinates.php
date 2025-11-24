932df5f07a94303ad3819a046bf6cfd14e166bc3
Jose Terrazas <jozaguts@gmail.com>
2025-11-24 02:15:47 -0600

    feat(object value): Coordiantes

---
+++ app/Domain/Properties/ValueObjects/Coordinates.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Domain\Properties\ValueObjects;
+
+class Coordinates
+{
+    public function __construct(public readonly float $latitude, public readonly float $longitude)
+    {
+        if($this->latitude < -90 || $this->latitude > 90) {
+            throw new \InvalidArgumentException('Latitude must be between -90 and 90 degrees.');
+        }
+        if($this->longitude < -180 || $this->longitude > 180) {
+            throw new \InvalidArgumentException('Longitude must be between -180 and 180 degrees.');
+        }
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new self($data['latitude'], $data['longitude']);
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+        ];
+    }
+
+
+}
