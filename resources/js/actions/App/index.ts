06d0c692102f1eabe053016932327cd77e6f30c6
Jose Terrazas <jozaguts@gmail.com>
2025-12-23 14:30:34 -0600

    feat(filament): feature Crud added

--- resources/js/actions/App/index.ts
+++ resources/js/actions/App/index.ts
@@ -1,7 +1,9 @@
+import Filament from './Filament'
 import Interface from './Interface'
 import Http from './Http'
 
 const App = {
+    Filament: Object.assign(Filament, Filament),
     Interface: Object.assign(Interface, Interface),
     Http: Object.assign(Http, Http),
 }
---
+++ resources/js/routes/filament/admin/resources/index.ts
@@ -0,0 +1,7 @@
+import features from './features'
+
+const resources = {
+    features: Object.assign(features, features),
+}
+
+export default resources
