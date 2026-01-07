45284f3bece4a70c79207769ee10bdcb3553a15f
Jose Terrazas <jozaguts@gmail.com>
2026-01-07 16:07:13 -0600

    feat(billing): init cashier

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
@@ -13,7 +14,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasUuids, HasFactory, Notifiable, TwoFactorAuthenticatable;
+    use HasUuids, HasFactory, Notifiable, TwoFactorAuthenticatable, Billable;
     public $incrementing = false;
     protected $keyType = 'string';
 
--- database/migrations/2026_01_07_214716_create_subscription_items_table.php
+++ database/migrations/2026_01_07_214716_create_subscription_items_table.php
@@ -11,19 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('subscriptions', function (Blueprint $table) {
+        Schema::create('subscription_items', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
-            $table->string('type');
+            $table->foreignId('subscription_id');
             $table->string('stripe_id')->unique();
-            $table->string('stripe_status');
-            $table->string('stripe_price')->nullable();
+            $table->string('stripe_product');
+            $table->string('stripe_price');
             $table->integer('quantity')->nullable();
-            $table->timestamp('trial_ends_at')->nullable();
-            $table->timestamp('ends_at')->nullable();
             $table->timestamps();
 
-            $table->index(['user_id', 'stripe_status']);
+            $table->index(['subscription_id', 'stripe_price']);
         });
     }
 
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('subscriptions');
+        Schema::dropIfExists('subscription_items');
     }
 };
